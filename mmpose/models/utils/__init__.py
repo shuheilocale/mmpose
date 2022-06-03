bbb899e604096f4eefe8b5829b6074986c36f7e4
Philipp Allgeuer <5592992+pallgeuer@users.noreply.github.com>
2022-06-03 16:10:59 +0200

    Fix warning with torch.meshgrid (#1402)
    * Fix warning with torch.meshgrid
    
    * Add meshgrid wrapper that is sensitive to torch version
    
    * move misc.py from mmpose.utils to mmpose.model.utils
    
    Co-authored-by: ly015 <liyining0712@gmail.com>

--- mmpose/models/utils/__init__.py
+++ mmpose/models/utils/__init__.py
@@ -1,10 +1,15 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .check_and_update_config import check_and_update_config
 from .ckpt_convert import pvt_convert
+from .geometry import batch_rodrigues, quat_to_rotmat, rot6d_to_rotmat
+from .misc import torch_meshgrid_ij
+from .ops import resize
+from .realnvp import RealNVP
 from .smpl import SMPL
 from .transformer import PatchEmbed, PatchMerging, nchw_to_nlc, nlc_to_nchw
 
 __all__ = [
     'SMPL', 'PatchEmbed', 'nchw_to_nlc', 'nlc_to_nchw', 'pvt_convert',
-    'PatchMerging'
+    'PatchMerging', 'batch_rodrigues', 'quat_to_rotmat', 'rot6d_to_rotmat',
+    'resize', 'RealNVP', 'torch_meshgrid_ij'
 ]
