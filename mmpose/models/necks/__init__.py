5d021895e60b8317ece9430f9fd1fff22ebaa8b7
zengwang430521 <zengwang430521@gmail.com>
2022-06-28 20:56:35 +0800

    add tcformer (#1447)

--- mmpose/models/necks/__init__.py
+++ mmpose/models/necks/__init__.py
@@ -3,7 +3,12 @@ from .fmap_proc_neck import FeatureMapProcessor
 from .fpn import FPN
 from .gap_neck import GlobalAveragePooling
 from .posewarper_neck import PoseWarperNeck
+from .tcformer_mta_neck import MTA
 
+<<<<<<< HEAD
 __all__ = [
     'GlobalAveragePooling', 'PoseWarperNeck', 'FPN', 'FeatureMapProcessor'
 ]
+=======
+__all__ = ['GlobalAveragePooling', 'PoseWarperNeck', 'FPN', 'MTA']
+>>>>>>> a8c23bf0 (add tcformer (#1447))
--- mmpose/models/utils/__init__.py
+++ mmpose/models/utils/__init__.py
@@ -1,15 +1,24 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+<<<<<<< HEAD
 from .check_and_update_config import check_and_update_config
 from .ckpt_convert import pvt_convert
+=======
+from .ckpt_convert import pvt_convert, tcformer_convert
+>>>>>>> a8c23bf0 (add tcformer (#1447))
 from .geometry import batch_rodrigues, quat_to_rotmat, rot6d_to_rotmat
 from .misc import torch_meshgrid_ij
 from .ops import resize
 from .realnvp import RealNVP
 from .smpl import SMPL
+from .tcformer_utils import (TCFormerDynamicBlock, TCFormerRegularBlock,
+                             TokenConv, cluster_dpc_knn, merge_tokens,
+                             token2map, token_interp)
 from .transformer import PatchEmbed, PatchMerging, nchw_to_nlc, nlc_to_nchw
 
 __all__ = [
     'SMPL', 'PatchEmbed', 'nchw_to_nlc', 'nlc_to_nchw', 'pvt_convert',
     'PatchMerging', 'batch_rodrigues', 'quat_to_rotmat', 'rot6d_to_rotmat',
-    'resize', 'RealNVP', 'torch_meshgrid_ij'
+    'resize', 'RealNVP', 'torch_meshgrid_ij', 'token2map', 'TokenConv',
+    'TCFormerRegularBlock', 'TCFormerDynamicBlock', 'cluster_dpc_knn',
+    'merge_tokens', 'token_interp', 'tcformer_convert'
 ]
